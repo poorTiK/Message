36db33d60bd76da0919bff76c87f76dabd449b31
Arkadii <sh3rgame@gmail.com>
2019-03-17 14:33:50 +0200

    group wnd

--- Message/CreateChatGroupWnd.xaml.cs
+++ Message/CreateChatGroupWnd.xaml.cs
@@ -1,27 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Message.Interfaces;
+using Message.ViewModel;
 
 namespace Message
 {
     /// <summary>
     /// Interaction logic for CreateChatGroupWnd.xaml
     /// </summary>
-    public partial class CreateChatGroupWnd : Window
+    public partial class CreateChatGroupWnd : Window, IView
     {
         public CreateChatGroupWnd()
         {
             InitializeComponent();
+
+            DataContext = new CreateChatGroupWndVM(this);
+        }
+
+        public void AnimatedResize(int h, int w)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseWindow()
+        {
+            Close();
+        }
+
+        public void Hide(bool isVisible)
+        {
+            if (isVisible)
+            {
+                Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Visibility = Visibility.Hidden;
+            }
+        }
+
+        public void SetOpacity(double opasity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Message/ViewModel/CreateChatGroupWndVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Message.Interfaces;
+
+namespace Message.ViewModel
+{
+    class CreateChatGroupWndVM : BaseViewModel
+    {
+        private IView _view;
+
+        public CreateChatGroupWndVM(IView view)
+        {
+            _view = view;
+        }
+
+
+    }
+}
